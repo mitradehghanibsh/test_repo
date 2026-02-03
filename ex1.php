735e16c860628ac01bef2168ee8f4989c80c80f8
mitra dehghani beshneh <mitra.dehghanibsh@gmail.com>
2026-02-03 22:51:46 +0200

    First exercise
    First Exercise

---
+++ ex1.php
@@ -0,0 +1,49 @@
+<html>
+    <body>
+        <h3>"Hello World!";<h3>
+    </body>
+</html>
+<?php
+echo "My name is Mitra";
+?>
+<h4>
+   <?php
+   $title = "PHP is intresting.";
+echo $title;
+?>
+</h4>
+
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 3;
+?>
+
+<table border="5" cellpadding="8">
+    <tr>
+        <th>S.n.</th>
+        <th>Name</th>
+        <th>Grade</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Mitra</td>
+        <td><?php echo $g1; ?></td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Mehrsam</td>
+        <td><?php echo $g2; ?></td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Neky</td>
+        <td><?php echo $g3; ?></td>
+    </tr>
+   
+    </tr>
+
+   
+</table>
+ <div>
+        <img src="screenshot.png" width="70%" > </div>
